76adf435f5c09d9581f5853a667cbaae9b23b4af
attn1 <attn1.repo@gmail.com>
2013-05-30 02:49:33 -0400

    Fix build.prop build

--- cm.mk
+++ cm.mk
@@ -9,6 +9,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflte BUILD_FINGERPRINT="custom" PRIVATE_BUILD_DESC="custom"
+
 PRODUCT_NAME := cm_jflte
 PRODUCT_DEVICE := jflte
 
--- variants/customizeVariant.sh
+++ variants/customizeVariant.sh
@@ -38,18 +38,18 @@ if [ $? = 0 ];
        sed -e 's|ro.product.model=jflte|ro.product.model=SGH-I337|g' /system/build.prop > temp && move temp /system/build.prop
        sed -e 's|ro.product.device=jflte|ro.product.device=jflteatt|g' /system/build.prop > temp && move temp /system/build.prop
        sed -e 's|ro.cm.device=jflt|ro.cm.device=jflteatt|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.01||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.02|# system.prop for SGH-I337|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.03|#|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.04|rild.libargs=-d /dev/smd0|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.05||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.06|# Fields for custom RIL|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.07|# MAX PROP NAME is 31 chars ---|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.08|ro.telephony.ril_class=SamsungQualcommUiccRIL|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.09|telephony.lteOnGsmDevice=1|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.10|ro.telephony.default_network=9|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.11||g' /system/build.prop > temp && move temp /system/build.prop
-       awk '!/build.prop.custom/' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.01||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.02|# system.prop for SGH-I337|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.03|#|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.04|rild.libargs=-d /dev/smd0|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.05||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.06|# Fields for custom RIL|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.07|# MAX PROP NAME is 31 chars ---|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.08|ro.telephony.ril_class=SamsungQualcommUiccRIL|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.09|telephony.lteOnGsmDevice=1|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.10|ro.telephony.default_network=9|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.11||g' /system/build.prop > temp && move temp /system/build.prop
+       awk '!/#build.prop.custom/' /system/build.prop > temp && move temp /system/build.prop
 fi
 
 #vzw
@@ -68,29 +68,29 @@ if [ $? = 0 ];
        sed -e 's|ro.product.model=jflte|ro.product.model=SGH-I545|g' /system/build.prop > temp && move temp /system/build.prop
        sed -e 's|ro.product.device=jflte|ro.product.device=jfltevzw|g' /system/build.prop > temp && move temp /system/build.prop
        sed -e 's|ro.cm.device=jflt|ro.cm.device=jfltevzw|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.01||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.02|# system.prop for SCH-I545|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.03|#|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.04|rild.libargs=-d /dev/smd0|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.05||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.06|# Fields for custom RIL|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.07|# MAX PROP NAME is 31 chars ---|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.08|ro.telephony.ril_class=SamsungCDMAQualcommRIL|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.09|ro.cdma.home.operator.numeric=310004|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.10|ro.cdma.home.operator.alpha=Verizon|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.11|ro.telephony.default_network=8|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.12|telephony.lteOnCdmaDevice=1|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.13||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.14|# Moar ril props|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.15|persist.radio.snapshot_enabled=1|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.16|persist.radio.snapshot_timer=22|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.17|ro.config.multimode_cdma=1|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.18|ro.config.combined_signal=true|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.19|ro.gsm.data_retry_config=max_retries=infinite,5000,5000,60000,120000,480000,900000|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.20|DEVICE_PROVISIONED=1|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.21|persist.eons.enabled=false|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.22||g' /system/build.prop > temp && move temp /system/build.prop
-       awk '!/build.prop.custom/' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.01||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.02|# system.prop for SCH-I545|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.03|#|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.04|rild.libargs=-d /dev/smd0|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.05||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.06|# Fields for custom RIL|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.07|# MAX PROP NAME is 31 chars ---|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.08|ro.telephony.ril_class=SamsungCDMAQualcommRIL|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.09|ro.cdma.home.operator.numeric=310004|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.10|ro.cdma.home.operator.alpha=Verizon|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.11|ro.telephony.default_network=8|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.12|telephony.lteOnCdmaDevice=1|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.13||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.14|# Moar ril props|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.15|persist.radio.snapshot_enabled=1|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.16|persist.radio.snapshot_timer=22|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.17|ro.config.multimode_cdma=1|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.18|ro.config.combined_signal=true|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.19|ro.gsm.data_retry_config=max_retries=infinite,5000,5000,60000,120000,480000,900000|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.20|DEVICE_PROVISIONED=1|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.21|persist.eons.enabled=false|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.22||g' /system/build.prop > temp && move temp /system/build.prop
+       awk '!/#build.prop.custom/' /system/build.prop > temp && move temp /system/build.prop
 fi
 
 #can
@@ -100,24 +100,24 @@ if [ $? = 0 ];
        cd /tmp
        dd if=/tmp/boot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot
        #customize build.prop
-       sed -e 's|ro.build.fingerprint=custom|ro.build.fingerprint=samsung/jfltevl/jfltecan:4.2.2/JDQ39/I337MVLUAMDJ:user/release-keys|g' build.prop > build.tmp
-       sed -e 's|ro.build.description=custom|ro.build.description=jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys|g' build.tmp > build.prop
+       sed -e 's|ro.build.fingerprint=variantcustom|ro.build.fingerprint=samsung/jfltevl/jfltecan:4.2.2/JDQ39/I337MVLUAMDJ:user/release-keys|g' build.prop > build.tmp
+       sed -e 's|ro.build.description=variantcustom|ro.build.description=jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys|g' build.tmp > build.prop
        sed -e 's|PRODUCT_DEVICE := jflte|PRODUCT_DEVICE := jfltecan|g' build.prop > build.tmp 
        sed -e 's|ro.product.model=jflte|ro.product.model=SGH-I337M|g' build.tmp > build.prop
        sed -e 's|ro.product.device=jflte|ro.product.device=jfltecan|g' build.prop > build.tmp
        sed -e 's|ro.cm.device=jflt|ro.cm.device=jfltecan|g' build.tmp > build.tmp
-       sed -e 's|build.prop.custom.01||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.02|# system.prop for SGH-I337M|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.03|#|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.04|rild.libargs=-d /dev/smd0|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.05||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.06|# Fields for custom RIL|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.07|# MAX PROP NAME is 31 chars ---|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.08|ro.telephony.ril_class=SamsungQualcommUiccRIL|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.09|telephony.lteOnGsmDevice=1|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.10|ro.telephony.default_network=9|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.11||g' /system/build.prop > temp && move temp /system/build.prop
-       awk '!/build.prop.custom/' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.01||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.02|# system.prop for SGH-I337M|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.03|#|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.04|rild.libargs=-d /dev/smd0|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.05||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.06|# Fields for custom RIL|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.07|# MAX PROP NAME is 31 chars ---|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.08|ro.telephony.ril_class=SamsungQualcommUiccRIL|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.09|telephony.lteOnGsmDevice=1|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.10|ro.telephony.default_network=9|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.11||g' /system/build.prop > temp && move temp /system/build.prop
+       awk '!/#build.prop.custom/' /system/build.prop > temp && move temp /system/build.prop
 fi
 
 #tmo
@@ -133,18 +133,18 @@ if [ $? = 0 ];
        sed -e 's|ro.product.model=jflte|ro.product.model=SGH-M919|g' build.tmp > build.prop
        sed -e 's|ro.product.device=jflte|ro.product.device=jfltetmo|g' build.prop > build.tmp
        sed -e 's|ro.cm.device=jflt|ro.cm.device=jfltetmo|g' build.tmp > build.tmp
-       sed -e 's|build.prop.custom.01||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.02|# system.prop for SGH-M919|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.03|#|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.04|rild.libargs=-d /dev/smd0|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.05||g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.06|# Fields for custom RIL|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.07|# MAX PROP NAME is 31 chars ---|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.08|ro.telephony.ril_class=SamsungQualcommUiccRIL|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.09|telephony.lteOnGsmDevice=1|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.10|ro.telephony.default_network=9|g' /system/build.prop > temp && move temp /system/build.prop
-       sed -e 's|build.prop.custom.11||g' /system/build.prop > temp && move temp /system/build.prop
-       awk '!/build.prop.custom/' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.01||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.02|# system.prop for SGH-M919|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.03|#|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.04|rild.libargs=-d /dev/smd0|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.05||g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.06|# Fields for custom RIL|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.07|# MAX PROP NAME is 31 chars ---|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.08|ro.telephony.ril_class=SamsungQualcommUiccRIL|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.09|telephony.lteOnGsmDevice=1|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.10|ro.telephony.default_network=9|g' /system/build.prop > temp && move temp /system/build.prop
+       sed -e 's|#build.prop.custom.11||g' /system/build.prop > temp && move temp /system/build.prop
+       awk '!/#build.prop.custom/' /system/build.prop > temp && move temp /system/build.prop
 fi
 
 # cleanup
